3295d4da2d417f7c240f99af4e41a25c37379119
Avishka K S <120089741+SandaruAvishka@users.noreply.github.com>
2024-02-07 22:28:45 +0530

    first commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\android sdk\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\android sdk\\fullter original practice\\first_project\\lib\\flutter_application_1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\android sdk\\flutter\\flutter"
+  "PROJECT_DIR=E:\\android sdk\\fullter original practice\\first_project\\lib\\flutter_application_1"
+  "FLUTTER_ROOT=E:\\android sdk\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\android sdk\\fullter original practice\\first_project\\lib\\flutter_application_1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\android sdk\\fullter original practice\\first_project\\lib\\flutter_application_1"
+  "FLUTTER_TARGET=E:\\android sdk\\fullter original practice\\first_project\\lib\\flutter_application_1\\lib\\main5.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\android sdk\\fullter original practice\\first_project\\lib\\flutter_application_1\\.dart_tool\\package_config.json"
+)
